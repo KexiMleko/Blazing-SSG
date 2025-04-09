79eca54ba01753e13165876e76cffe76c6ee4b0a
Keximleko <ocokolja.aleksa@gmail.com>
2025-04-09 22:53:20 +0200

    directory traversal and tl introduction

---
+++ src/component.rs
@@ -0,0 +1,7 @@
+pub struct Component{
+    selector:String,
+    className:String,    
+    htmlPath:Path,
+    jsPath:Path,
+    cssPath:Path,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::{
     env,
-    fs::{self, File},
+    fs::{self, DirEntry, File},
     io::{self, Write},
+    path::Path,
 };
+use tl::parse;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,7 +15,9 @@ fn main() {
     }
     match args[1].to_lowercase().as_str() {
         "build" => {
-            build();
+            if let Err(e) = build() {
+                eprintln!("Error occured while trying to build! - {} -", e)
+            }
         }
         "create" => {
             if args.len() < 3 as usize {
@@ -49,8 +53,12 @@ fn main() {
     }
 }
 
-fn build() {
+fn build() -> std::io::Result<()> {
     println!("Build started!");
+    let current_dir = env::current_dir()?;
+    visit_dirs(&current_dir.join("pages"), &get_page)?;
+    visit_dirs(&current_dir.join("components"), &perform_extension_action)?;
+    Ok(())
 }
 fn create_component(component_name: String) -> std::io::Result<()> {
     let config_path = env::current_dir()?.join("blazing-config.json");
@@ -65,9 +73,24 @@ fn create_component(component_name: String) -> std::io::Result<()> {
 
     fs::create_dir(&new_dir)?;
 
-    let mut html_file = File::create(new_dir.join(format!("{}-component.html", &component_name)));
-    let mut js_file = File::create(new_dir.join(format!("{}-component.js", &component_name)));
-    let mut css_file = File::create(new_dir.join(format!("{}-component.css", &component_name)));
+    let mut html_file = File::create(new_dir.join(format!("{}.component.html", &component_name)))?;
+    let mut js_file = File::create(new_dir.join(format!("{}.component.js", &component_name)))?;
+    File::create(new_dir.join(format!("{}.component.css", &component_name)))?;
+    let html_text = format!("{} works", component_name);
+    let js_text = format!(
+        r#"@Component({{
+  selector: 'com-{}',
+  templateUrl: './{}.component.html',
+  styleUrls: ['./{}.component.css']
+}})
+  
+  class {}Component{{
+  
+  }}"#,
+        component_name, component_name, component_name, component_name
+    );
+    html_file.write(html_text.as_bytes())?;
+    js_file.write(js_text.as_bytes())?;
     println!("Component created! {}", component_name);
     return Ok(());
 }
@@ -93,7 +116,7 @@ fn create_project(project_name: String) -> std::io::Result<()> {
     let mut config_file = File::create(config_path)?;
     let config_text = format!(
         r#"{{
-  "project_name": {},
+  "project_name": "{}",
   "output_dir": "output",
   "components_dir": "components",
   "pages_dir": "pages"
@@ -102,5 +125,42 @@ fn create_project(project_name: String) -> std::io::Result<()> {
     );
     config_file.write(config_text.as_bytes())?;
 
+    return Ok(());
+}
+fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)->std::io::Result<()>) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path, cb)?;
+            } else {
+                cb(&entry)?;
+            }
+        }
+    }
+    Ok(())
+}
+fn get_page(entry: &DirEntry) -> std::io::Result<()> {
+    if let Some(ext) = entry.path().extension().and_then(|e| e.to_str()) {
+        match ext {
+            "html" => {
+                let html = fs::read_to_string(entry.path())?;
+                let document = parse(&html, tl::ParserOptions::default());
+            }
+            _ => {}
+        }
+    }
+    return Ok(());
+}
+fn perform_extension_action(entry: &DirEntry) ->std::io::Result<()>{
+    if let Some(ext) = entry.path().extension().and_then(|e| e.to_str()) {
+        match ext {
+            "js" => {}
+            "html" => {}
+            "css" => {}
+            _ => {}
+        }
+    }
     return Ok(());
 }
